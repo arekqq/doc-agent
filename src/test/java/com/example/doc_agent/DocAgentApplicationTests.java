2a3cdf44826e64b832ea263c5d5d59c97781ccac
Arkadiusz Rogacki <arek.rogacki@gmail.com>
2024-10-14 15:42:24 +0200

    project structure from spring initializr

---
+++ src/test/java/com/example/doc_agent/DocAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doc_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
